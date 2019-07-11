f304b262ebc2868ba1c81a3863fbc8609160149b
MomchilAngelovv <momchilangelov0040@gmail.com>
2019-07-11 14:41:33 +0300

    Small bug fix

--- WowGuildManager/WowGuildManager.Web/Startup.cs
+++ WowGuildManager/WowGuildManager.Web/Startup.cs
@@ -63,8 +63,8 @@ namespace WowGuildManager.Web
 
             services.AddAuthentication().AddFacebook(options =>
             {
-                options.AppId = "440150153493696";
-                options.AppSecret = "7b330adc49cae00c5b11d5e58e0caa77";
+                options.AppId = this.Configuration["Facebook:AppId"];
+                options.AppSecret = this.Configuration["Facebook:AppSecret"];
             });
 
             services.AddAutoMapper(typeof(Startup));
